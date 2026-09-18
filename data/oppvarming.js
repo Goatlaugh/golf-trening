e245925097b6eb09c6a5fc7f129fd06681898c4c
Goatlaugh <geitlebendik@gmail.com>
2026-09-18 15:10:23 +0200

    Update oppvarming.js
    oppdatere oppvarming til å inkluderer videoFile

--- data/oppvarming.js
+++ data/oppvarming.js
@@ -3,7 +3,7 @@ const oppvarming = [
 navn:"Bekkentilt", 
 tid:"Tid: 1 min", 
 fokus:["Beveg bekken frem/tilbake","Stabil overkropp"], 
-bilde:"images/gifs/oppvarming/bekkentilt.gif",
+videoFile:"images/videos/oppvarming/bekkentilt.mp4",
 timer: 60,          // 1 minute timer
 autoNext: 1000,      // go to next exercise 1 sec after timer finishes
 autoStartDelay: 5000 // start timer automatically after 5 sec
@@ -13,7 +13,7 @@ autoStartDelay: 5000 // start timer automatically after 5 sec
 navn:"Sidebøy rygg", 
 tid:"Tid: 1 min", 
 fokus:["Bøy side til side","Unngå rotasjon"], 
-bilde:"images/gifs/oppvarming/sideboy.gif",
+videoFile:"images/videos/oppvarming/sideboy.mp4",
 timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
@@ -22,7 +22,7 @@ autoStartDelay: 5000
 {
 navn:"Hofte/rygg separasjon", 
 tid:"Tid: 1 min", fokus:["Roter motsatt vei","Øk mobilitet"], 
-bilde:"images/gifs/oppvarming/hofte.gif",
+videoFile:"images/videos/oppvarming/hofte.mp4",
 timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
@@ -31,7 +31,7 @@ autoStartDelay: 5000
 {
 navn:"Skulderrotasjon", 
 tid:"Tid: 1 min", fokus:["Rolig rotasjon","Stabil kjerne"], 
-bilde:"images/gifs/oppvarming/skulder.gif",
+videoFile:"images/videos/oppvarming/skulder.mp4",
 timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
@@ -41,7 +41,7 @@ autoStartDelay: 5000
 navn:"Rotasjon mot pinne", 
 tid:"Tid: 1 min", 
 fokus:["Hold vinkler stabile","Kontrollert rotasjon"], 
-bilde:"images/gifs/oppvarming/rotasjon.gif",
+videoFile:"images/videos/oppvarming/rotasjon.mp4",
 timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
@@ -51,7 +51,7 @@ autoStartDelay: 5000
 navn:"Utoverrotasjon skulder", 
 tid:"Tid: 30 sec", 
 fokus:["Aktiver bakside skulder","Kontrollert bevegelse"], 
-bilde:"images/gifs/oppvarming/utover.gif",
+videoFile:"images/videos/oppvarming/utover.mp4",
 timer: 30,
 autoNext:1000,
 autoStartDelay: 5000
@@ -61,7 +61,7 @@ autoStartDelay: 5000
 navn:"Helikopter", 
 tid:"Tid: 1 min & 30 sec", 
 fokus:["Dynamisk rotasjon","Aktiver overkropp"], 
-bilde:"images/gifs/oppvarming/helikopter.gif",
+videoFile:"images/videos/oppvarming/helikopter.mp4",
 timer: 90,
 autoNext:1000,
 autoStartDelay: 5000
