ec34ad6a02b71e7d7d42260ae163377d5ddcfcb8
Goatlaugh <geitlebendik@gmail.com>
2026-09-18 10:28:49 +0200

    Update oppvarming.js
    ny oppdatering med tid

--- data/oppvarming.js
+++ data/oppvarming.js
@@ -1,67 +1,68 @@
 const oppvarming = [
 {
 navn:"Bekkentilt", 
-tid:"Tid: 2 min", 
+tid:"Tid: 1 min", 
 fokus:["Beveg bekken frem/tilbake","Stabil overkropp"], 
 bilde:"images/gifs/oppvarming/bekkentilt.gif",
-timer: 120,          // 2 minute timer
-autoNext: 1000,      // go to next exercise 3 sec after timer finishes
+timer: 60,          // 1 minute timer
+autoNext: 1000,      // go to next exercise 1 sec after timer finishes
 autoStartDelay: 5000 // start timer automatically after 5 sec
 },
 
 {
 navn:"Sidebøy rygg", 
-tid:"Tid: 2 min", 
+tid:"Tid: 1 min", 
 fokus:["Bøy side til side","Unngå rotasjon"], 
 bilde:"images/gifs/oppvarming/sideboy.gif",
-timer: 120,
+timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
 },
 
 {
 navn:"Hofte/rygg separasjon", 
-tid:"Tid: 2 min", fokus:["Roter motsatt vei","Øk mobilitet"], 
+tid:"Tid: 1 min", fokus:["Roter motsatt vei","Øk mobilitet"], 
 bilde:"images/gifs/oppvarming/hofte.gif",
-timer: 120,
+timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
 },
 
 {
 navn:"Skulderrotasjon", 
-tid:"Tid: 2 min", fokus:["Rolig rotasjon","Stabil kjerne"], 
+tid:"Tid: 1 min", fokus:["Rolig rotasjon","Stabil kjerne"], 
 bilde:"images/gifs/oppvarming/skulder.gif",
-timer: 120,
+timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
 },
 
 {
-navn:"Rotasjon mot pinne", tid:"Tid: 2 min", 
+navn:"Rotasjon mot pinne", 
+tid:"Tid: 1 min", 
 fokus:["Hold vinkler stabile","Kontrollert rotasjon"], 
 bilde:"images/gifs/oppvarming/rotasjon.gif",
-timer: 120,
+timer: 60,
 autoNext:1000,
 autoStartDelay: 5000
 },
 
 {
 navn:"Utoverrotasjon skulder", 
-tid:"Tid: 2 min", 
+tid:"Tid: 30 sec", 
 fokus:["Aktiver bakside skulder","Kontrollert bevegelse"], 
 bilde:"images/gifs/oppvarming/utover.gif",
-timer: 120,
+timer: 30,
 autoNext:1000,
 autoStartDelay: 5000
 },
 
 {
 navn:"Helikopter", 
-tid:"Tid: 2 min", 
+tid:"Tid: 1 min & 30 sec", 
 fokus:["Dynamisk rotasjon","Aktiver overkropp"], 
 bilde:"images/gifs/oppvarming/helikopter.gif",
-timer: 120,
+timer: 90,
 autoNext:1000,
 autoStartDelay: 5000
 }
