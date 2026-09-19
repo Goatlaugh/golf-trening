312758e26a9edf4d3bd23cc01432fb10f2490b2c
Goatlaugh <geitlebendik@gmail.com>
2026-09-19 13:30:33 +0200

    Add files via upload
    adding styrke.js

---
+++ data/styrke.js
@@ -0,0 +1,281 @@
+const styrke = [
+
+{  navn: "Pushup",
+
+  muskelgruppe: ["Bryst",
+                 "Skuldre",
+    		 "Triceps",
+    		 "Kjernemuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: [ "Armer og skuldre", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/pushup.mp4"
+},
+
+
+
+{
+  navn: "Bekkenhev",
+
+  muskelgruppe: ["Setemuskulatur",
+    		 "Bakside lår",
+    		 "Kjernemuskulatur",
+    		 "Korsrygg"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Ben", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/bekkenhev.mp4"
+},
+
+
+
+{
+  navn: "Rulle ned og opp",
+
+  muskelgruppe: ["Mage",
+    		 "Kjernemuskulatur",
+    		 "Hoftebøyere"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Mage", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/rulle.mp4"
+},
+
+
+
+{
+  navn: "Deadbug",
+
+  muskelgruppe: ["Mage",
+    		 "Dyp kjernemuskulatur",
+    		 "Hoftebøyere"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Mage", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/deadbug.mp4"
+},
+
+
+
+{
+  navn: "Planken med rotasjon",
+
+  muskelgruppe: ["Kjernemuskulatur",
+    		 "Skrå magemuskler",
+		 "Skuldre",
+    		 "Stabiliseringsmuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Kjernemuskulatur", "Mage"],
+
+  videoFile: "images/videos/styrke/planken.mp4"
+},
+
+
+
+{
+  navn: "Stuperen",
+
+  muskelgruppe: ["Setemuskulatur",
+    		 "Bakside lår",
+    		 "Kjernemuskulatur",
+    		 "Balanse og stabilitet"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Ben", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/stuperen.mp4"
+},
+
+
+
+{
+  navn: "Kålorm",
+
+  muskelgruppe: ["Kjernemuskulatur",
+    		 "Skuldre",
+     		 "Bakside lår",
+    		 "Bevegelighet"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Kjernemuskulatur", "Armer og skuldre"],
+
+  videoFile: "images/videos/styrke/kaalorm.mp4"
+},
+
+
+
+{
+  navn: "Knebøy",
+
+  muskelgruppe: ["Forside lår",
+    		 "Setemuskulatur",
+    		 "Bakside lår",
+    		 "Kjernemuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Ben", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/kneboey.mp4"
+},
+
+
+
+{
+  navn: "Utfall bakover med rotasjon",
+
+  muskelgruppe: ["Setemuskulatur",
+    		 "Forside lår",
+    		 "Kjernemuskulatur",
+    		 "Skrå magemuskler"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Ben", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/utfall.mp4"
+},
+
+
+
+{
+  navn: "Lateralt utfall",
+
+  muskelgruppe: ["Setemuskulatur",
+    		 "Innside lår",
+    		 "Utside hofte",
+   		 "Forside lår"],
+
+  repetisjon: "10 x 3 sett",
+
+  styrkeTag: ["Ben"],
+
+  videoFile: "images/videos/styrke/lateralt.mp4"
+},
+
+
+
+{
+  navn: "Bulgarsk utfall",
+
+  muskelgruppe: ["Setemuskulatur",
+   		 "Forside lår",
+   		 "Bakside lår",
+   		 "Kjernemuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Stol"],
+
+  styrkeTag: ["Ben", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/bulgarsk.mp4"
+},
+
+
+
+{
+  navn: "Stående tåhev",
+
+  muskelgruppe: ["Legger"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Stol"],
+
+  styrkeTag: ["Ben"],
+
+  videoFile: "images/videos/styrke/taahev.mp4"
+},
+
+
+
+{
+  navn: "Stående skulderpress",
+
+  muskelgruppe: ["Skuldre",
+   		 "Triceps",
+   		 "Øvre rygg",
+   		 "Kjernemuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Strikk"],
+
+  styrkeTag: ["Armer og skuldre", "Kjernemuskulatur"],
+
+  videoFile: "images/videos/styrke/skulderpress.mp4"
+},
+
+
+
+{
+  navn: "Horisontal armsving",
+
+  muskelgruppe: ["Bakside skuldre",
+   		 "Øvre rygg",
+   		 "Armer"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Strikk"],
+
+  styrkeTag: ["Armer og skuldre", "Rygg"],
+
+  videoFile: "images/videos/styrke/armsving.mp4"
+},
+
+
+
+{
+  navn: "Roing med en arm",
+
+  muskelgruppe: ["Latissimus",
+   		 "Øvre rygg",
+   		 "Bakside skuldre",
+   		 "Biceps",
+   		 "Kjernemuskulatur"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Strikk"],
+
+  styrkeTag: ["Rygg", "Armer og skuldre"],
+
+  videoFile: "images/videos/styrke/roing.mp4"
+},
+
+
+
+{
+  navn: "Sittende roing",
+
+  muskelgruppe: ["Latissimus",
+   		 "Øvre rygg",
+   		 "Bakside skuldre",
+   		 "Biceps"],
+
+  repetisjon: "10 x 3 sett",
+
+  utstyr: ["Strikk"],
+
+  styrkeTag: ["Rygg", "Armer og skuldre"],
+
+  videoFile: "images/videos/styrke/sittende roing.mp4"
+}
+
+
+
+];
