bd3215aad8aadcfa173a406a8ab774376f5a36ff
Goatlaugh <geitlebendik@gmail.com>
2026-09-19 13:31:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let currentList = [];
 let currentIndex = 0;
 let currentScreen = "main";
+let activeStrengthFilter = "Alle";
 let timerInterval;
 let remainingTime = 0;
 let totalTime = 0;
@@ -28,8 +29,10 @@ function createMenu() {
 function openCategory(cat) {
   const category = categories.find(c => c.id === cat);
 
+activeStrengthFilter = "Alle";
 currentList = category.data;
   currentScreen = "category";
+
 
   let html = "<h2>" + cat.toUpperCase() + "</h2>";
 
@@ -50,6 +53,59 @@ if (cat === "oppvarming") {
 `;
 }
 
+if (cat === "styrke") {
+
+ html += `
+
+  <div class="strength-all">
+
+    <button
+  class="${activeStrengthFilter === 'Alle' ? 'active-filter' : ''}"
+  onclick="filterStrength('Alle')">
+      Alle Øvelser
+    </button>
+
+  </div>
+
+  <div class="strength-filters">
+
+    <button
+  class="${activeStrengthFilter === 'Armer og skuldre' ? 'active-filter' : ''}"
+  onclick="filterStrength('Armer og skuldre')">
+      💪 Armer
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Kjernemuskulatur' ? 'active-filter' : ''}"
+    onclick="filterStrength('Kjernemuskulatur')">
+      🧠 Kjerne
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Rygg' ? 'active-filter' : ''}"
+    onclick="filterStrength('Rygg')">
+      🏋️ Rygg
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Ben' ? 'active-filter' : ''}"
+    onclick="filterStrength('Ben')">
+      🦵 Ben
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Mage' ? 'active-filter' : ''}"
+    onclick="filterStrength('Mage')">
+      🔥 Mage
+    </button>
+
+  </div>
+
+`;
+
+}
+
+
 
   currentList.forEach((e,i)=>{
    html += '<div class="card" onclick="openExercise(' + i + ')">' +
@@ -63,6 +119,97 @@ if (cat === "oppvarming") {
   show("category");
 }
 
+
+function filterStrength(tag) {
+
+  activeStrengthFilter = tag;
+
+  let exercises;
+
+  if (tag === "Alle") {
+
+    exercises = styrke;
+
+  } else {
+
+    exercises = styrke.filter(exercise =>
+      exercise.styrkeTag.includes(tag)
+    );
+
+  }
+
+  currentList = exercises;
+
+  let html = "<h2>STYRKE</h2>";
+
+  html += `
+
+  <div class="strength-all">
+
+  <button
+    class="${activeStrengthFilter === 'Alle' ? 'active-filter' : ''}"
+    onclick="filterStrength('Alle')">
+    Alle Øvelser
+  </button>
+
+</div>
+
+  <div class="strength-filters">
+
+    <button
+    class="${activeStrengthFilter === 'Armer og skuldre' ? 'active-filter' : ''}"
+    onclick="filterStrength('Armer og skuldre')">
+      💪 Armer
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Kjernemuskulatur' ? 'active-filter' : ''}"
+    onclick="filterStrength('Kjernemuskulatur')">
+      🧠 Kjerne
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Rygg' ? 'active-filter' : ''}"
+    onclick="filterStrength('Rygg')">
+      🏋️ Rygg
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Ben' ? 'active-filter' : ''}"
+    onclick="filterStrength('Ben')">
+      🦵 Ben
+    </button>
+
+    <button
+    class="${activeStrengthFilter === 'Mage' ? 'active-filter' : ''}"
+    onclick="filterStrength('Mage')">
+
+      🔥 Mage
+    </button>
+
+  </div>
+
+`;
+
+  exercises.forEach((e, i) => {
+
+    html += `
+      <div class="card" onclick="openExercise(${i})">
+
+        <span class="left">
+          ${e.navn}
+        </span>
+
+        <span class="arrow">›</span>
+
+      </div>
+    `;
+
+  });
+
+  document.getElementById("category").innerHTML = html;
+
+}
 
 function startOppvarming() {
 
@@ -82,7 +229,26 @@ function openExercise(i, autoStart = false) {
   currentIndex = i;
   currentScreen = "exercise";
 
+const exercise = document.getElementById("exercise");
+
+if (exercise) {
+  exercise.style.opacity = "0";
+}
+
+  setTimeout(() => {
+
   renderExercise();
+
+  const exercise =
+    document.getElementById("exercise");
+
+  if (exercise) {
+    exercise.style.opacity = "1";
+  }
+
+}, 150);
+
+
 
   if (autoStart) {
 
@@ -158,12 +324,75 @@ else if (e.bilde) {
         '</div>';
 
 html += "<h1>" + e.navn + "</h1>";
+  if (e.tid) {
   html += "<h2>" + e.tid + "</h2>";
+}
+
+if (e.styrkeTag) {
+
+  html += '<div class="tag-container">';
+
+  e.styrkeTag.forEach(tag => {
+
+    html += `
+      <span class="exercise-tag">
+        ${tag}
+      </span>
+    `;
+
+  });
+
+  html += "</div>";
+
+}
+
+
+if (e.muskelgruppe) {
+
+  html += "<b>Muskelgruppe:</b><ul>";
+
+  e.muskelgruppe.forEach(m => {
+    html += "<li>" + m + "</li>";
+  });
+
+  html += "</ul>";
+
+}
+
+if (e.repetisjon) {
+
+  html += "<b>Repetisjon:</b>";
+
+  html += "<div>" + e.repetisjon + "</div>";
+
+}
+
+if (e.utstyr) {
+
+  html += "<b>Utstyr:</b><ul>";
+
+  e.utstyr.forEach(u => {
+    html += "<li>" + u + "</li>";
+  });
+
+  html += "</ul>";
+
+}
+
+
+
+  if (e.fokus) {
+
   html += "<b>Fokus:</b><ul>";
-e.fokus.forEach(f => {  
-  html += "<li>" + f + "</li>";
-});
-html += "</ul>";
+
+  e.fokus.forEach(f => {
+    html += "<li>" + f + "</li>";
+  });
+
+  html += "</ul>";
+
+}
+
 
 if (e.sjekkpunkter) {
   html += "<b>Sjekkpunkter:</b><ul>";
