4ecf7bc915ad5b2e8800fca051883d321985b3a4
Goatlaugh <geitlebendik@gmail.com>
2026-09-23 13:04:25 +0200

    Update range.js

--- data/range.js
+++ data/range.js
@@ -22,23 +22,34 @@ fremgang:[
 "Steg 2: Sving 'L' til 'Y'",
 "Steg 3: 'L' til 'L'"
 ],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/halvsving-l-til-l.-y-til-l",
-bilde:"images/exercises/range/halvsving.jpg"
+videoFile:"images/videos/range/halvsving.mp4"
 },
 
 {navn:"Sving tauet", 
 tid:"Tid: 1 min",
 fokus:["Riktig tempo","God sekvens"],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/sving-tauet",
-bilde:"images/exercises/range/tau.jpg"
+videoFile:"images/videos/range/tau.mp4"
 },
 
 {navn:"Fleetwood - paraplyfinish", 
 tid:"Tid: 1 min",
 fokus:["Stabil finish","God balanse"],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/fleetwood-paraplyfinish",
-bilde:"images/exercises/range/finish.jpg"
+videoFile:"images/videos/range/finish.mp4"
 },
+
+{navn:"Take-away med bøtte / ballong", 
+tid:"Tid: 1 min",
+fokus:["Riktig start","Rett køllebane"],
+fremgang:[
+"Plasser bøtta mellom underarmene og utfør en take-away",
+"Dette er med på å skape:",
+"1. Køllebladet holder seg nøytralt",
+"2. Køllebladet er på utsiden av hendene i take-away",
+"3. Kropp og armer koblet sammen"
+],
+videoFile:"images/videos/range/takeaway.mp4"
+},
+
 
 {navn:"Svinge på en fot", 
 tid:"Tid: 5 min",
@@ -48,29 +59,15 @@ fremgang:[
 "Venstre svingretning: Ved å legge ballen bak i oppstillingen tvinger man seg til å tilte mer bakover i ryggsøylen. Denne bevegelsen er som regel utfordrende for en som svinger mye venstre.",
 "Høyre svingretning: Ved å legge ballen bak i oppstillingen tvinger man seg til å tilte mindre bakover i ryggsøylen. Denne bevegelsen er som regel utfordrende for en som svinger mye høyre."
 ],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/svingretningovelse?redirect=new",
-bilde:"images/exercises/range/enfot.jpg"
+videoFile:"images/videos/range/enfot.mp4"
 },
 
-{navn:"Take-away med bøtte / ballong", 
-tid:"Tid: 5 min",
-fokus:["Riktig start","Rett køllebane"],
-fremgang:[
-"Plasser bøtta mellom underarmene og utfør en take-away",
-"Dette er med på å skape:",
-"1. Køllebladet holder seg nøytralt",
-"2. Køllebladet er på utsiden av hendene i take-away",
-"3. Kropp og armer koblet sammen"
-],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/take-away-med-botte-ballong?redirect=new",
-bilde:"images/exercises/range/takeaway.jpg"
-},
 
 {navn:"Pegg mot pinne i baksving", 
-tid:"Tid: 10 min",
+tid:"Tid: 5 min",
 fokus:["Riktig baksving","God topp-posisjon"],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/pegg-mot-pinne-i-baksving?redirect=new",
-bilde:"images/exercises/range/baksving.jpg"
+utstyr:["Pegg", "Alignment Stick"],
+videoFile:"images/videos/range/baksving.mp4"
 },
 
 {navn:"Sving over håndkle", 
@@ -81,8 +78,7 @@ fremgang:[
 "Deretter er oppgaven å slå baller, uten at køllehodet treffer handduken",
 "Utføres dette riktig vil utøveren slå ned på ballen, noe som er en forutsetning for et godt balltreff uten pegg"
 ],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/sving-over-handduken",
-bilde:"images/exercises/range/handkle.jpg"
+videoFile:"images/videos/range/handkle.mp4"
 },
 
 {navn:"Planken - svingspor", 
@@ -94,8 +90,7 @@ fremgang:[
 "Plasser køllebladet ved ballen og legg planken ca. 1-2cm på utsiden av køllebladet",
 "Oppgaven er enkel: Slå ballen uten å treffe 'planken'"
 ],
-video:"https://ovelsesbank.golfforbundet.no/ovelser/alle-ovelser/planken-lange-slag-med-svingspor",
-bilde:"images/exercises/range/planke.jpg"
+videoFile:"images/videos/range/planke.mp4"
 },
 
 {navn:"Sving over pegg", 
